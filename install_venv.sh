89787820cac827163d31c3523f4a8d20a27c51c6
Jakub <jakub.creative@gmail.com>
2024-06-03 10:16:46 +0700

    fixed instal_venv.sh

--- install_venv.sh
+++ install_venv.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
-SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-cd $SCRIPT_DIR
+
+# Get the directory of the script
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" &> /dev/null && pwd)"
+cd "$SCRIPT_DIR" || exit
+
 # Create a virtual environment
 python -m venv venv || { echo "Failed to create venv"; exit 1; }
 
@@ -8,7 +11,7 @@ python -m venv venv || { echo "Failed to create venv"; exit 1; }
 source venv/bin/activate || { echo "Failed to activate venv"; exit 1; }
 
 # Install requirements
-pip install -r requirements.txt --no-cache|| { echo "Failed to install requirements"; exit 1; }
+pip install -r requirements.txt || { echo "Failed to install requirements"; exit 1; }
 
 # Check if .env file exists, if not copy .env_example to .env
 if [ ! -f "$SCRIPT_DIR/.env" ]; then
@@ -16,4 +19,4 @@ if [ ! -f "$SCRIPT_DIR/.env" ]; then
     cp "$SCRIPT_DIR/.env_example" "$SCRIPT_DIR/.env"
 fi
 
-echo "Installation completed successfully. Do not forget to update the .env file with your credentials. Then run run_venv.sh to start the app""
+echo "Installation completed successfully. Do not forget to update the .env file with your credentials. Then run run_venv.sh to start the app."
