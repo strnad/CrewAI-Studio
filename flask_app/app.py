8f235e9a11fc60e545b6646baf9e73d1b53b309c
bossm747 <admin@tmseven-47.com>
2024-11-09 04:48:27 +0000

    feat: add API endpoints for crews, tools, agents, tasks, and tools state

--- flask_app/app.py
+++ flask_app/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 from flask_cors import CORS
 
 app = Flask(__name__)
@@ -7,6 +7,26 @@ CORS(app)
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/api/crews', methods=['GET'])
+def get_crews():
+    return jsonify([])
+
+@app.route('/api/tools', methods=['GET'])
+def get_tools():
+    return jsonify([])
+
+@app.route('/api/agents', methods=['GET'])
+def get_agents():
+    return jsonify([])
+
+@app.route('/api/tasks', methods=['GET'])
+def get_tasks():
+    return jsonify([])
+
+@app.route('/api/tools/state', methods=['GET'])
+def get_tools_state():
+    return jsonify([])
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
