63ec0e65492bf8695c368ae0374f4e2a073b3c80
Jakub Strnad <jakub.creative@gmail.com>
2025-03-14 15:02:41 +0100

    bugfix: only relevant inputs are saved to results

--- app/pg_crew_run.py
+++ app/pg_crew_run.py
@@ -211,12 +211,26 @@ class PageCrewRun:
                     ss.saved_results = set()
                 
                 if result_identifier not in ss.saved_results:
+                    # FIXED: Only get placeholders related to the current run
+                    # Get only relevant placeholders for this specific crew
+                    relevant_placeholders = {}
+                    
+                    # First, extract all placeholders for the current crew
+                    curr_crew = self.get_mycrew_by_name(ss.selected_crew_name)
+                    if curr_crew:
+                        crew_placeholders = self.get_placeholders_from_crew(curr_crew)
+                        # Only include placeholders that were actually used in this crew
+                        for placeholder in crew_placeholders:
+                            placeholder_key = f'placeholder_{placeholder}'
+                            if placeholder_key in ss.placeholders:
+                                relevant_placeholders[placeholder_key] = ss.placeholders[placeholder_key]
+                    
                     # Create a new Result instance with serialized result
                     result = Result(
                         id=f"R_{rnd_id()}",
                         crew_id=ss.selected_crew_name,
                         crew_name=ss.selected_crew_name,
-                        inputs={key.split('_')[1]: value for key, value in ss.placeholders.items()},
+                        inputs={key.split('_')[1]: value for key, value in relevant_placeholders.items()},
                         result=self.serialize_result(ss.result)  # Serialize the result before saving
                     )
                     
@@ -235,11 +249,20 @@ class PageCrewRun:
                 st.expander("Full output", expanded=False).write(ss.result)
 
                 # Add print button
-                inputs = {key.split('_')[1]: value for key, value in ss.placeholders.items()}
+                # FIXED: Also use the relevant placeholders for the printable view
+                relevant_inputs = {}
+                curr_crew = self.get_mycrew_by_name(ss.selected_crew_name)
+                if curr_crew:
+                    crew_placeholders = self.get_placeholders_from_crew(curr_crew)
+                    for placeholder in crew_placeholders:
+                        placeholder_key = f'placeholder_{placeholder}'
+                        if placeholder_key in ss.placeholders:
+                            relevant_inputs[placeholder] = ss.placeholders[placeholder_key]
+                
                 html_content = generate_printable_view(
                     ss.selected_crew_name,
                     ss.result,
-                    inputs,
+                    relevant_inputs,
                     formatted_result
                 )
                 if st.button("Open Printable View"):
